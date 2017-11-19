0295b59fa9d593d6ac7ae2c36b5ac3f910f39f90
tpressley <trspres@gmail.com>
2017-11-18 21:53:39 -0500

    Added code-behind

---
+++ src/ESIConnection/ClassLibrary1/ESIConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ClassLibrary1
+{
+    public static class ESIConnection
+    {
+
+        public static string GetJsonFromUri(string uri)
+        {
+            string html = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            return html;
+        }
+    }
+
+}
