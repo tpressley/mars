e591380be4da0aacc1fef0913e89ac02d8b8bccb
tpressley <trspres@gmail.com>
2017-11-24 23:17:35 -0500

    Added hostile/friendly selection interface

---
+++ AdminPages/Admin.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Admin.aspx.cs" Inherits="AdminPages_Admin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="AllianceNameEntry" runat="server"></asp:TextBox>
+            <asp:Button ID="AddFriendly" runat="server" Text="Button" OnClick="AddFriendly_Click" />
+            <asp:Button ID="AddHostile" runat="server" Text="Button" OnClick="AddHostile_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ AdminPages/Admin.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using System.IO;
+public partial class AdminPages_Admin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void AddFriendly_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void AddHostile_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    public void AddEntryToCsv(string file,string text)
+    {
+        File.AppendAllText(file,text);
+    }
+}
